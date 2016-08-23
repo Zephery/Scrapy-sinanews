a8dd242c2c10a31577b064283b591554b1ca8379
Zephery <1570631036@qq.com>
2016-08-23 12:44:39 +0800

    almost finish

--- sinanews/spiders/Sinanews.py
+++ sinanews/spiders/Sinanews.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 import scrapy
 from bs4 import BeautifulSoup
 from sinanews.items import SinanewsItem
@@ -6,14 +7,14 @@ from sinanews.items import SinanewsItem
 class SinaSpider(scrapy.Spider):
     name = "sinanews"
     allowed_domains = ["news.sina.com.cn"]
-    start_urls = ["http://news.sina.com.cn/c/nd/2016-08-21/doc-ifxvcsrn8844418.shtml"]
+    start_urls = ["http://news.sina.com.cn"]
 
     # start_urls = ["http://news.sina.com.cn/"]
 
     def parse(self, response):
-        for url in response.xpath('//div[@class="feed-card-item"]/h2/a/@href').extract():
-            # scrapy.Request(url, callback=self.parseNews)
+        for url in response.xpath('//li/a/@href').extract():
             yield scrapy.Request(url, callback=self.parseNews)
+            yield scrapy.Request(url)
 
     def parseNews(self, response):
         News = []
@@ -26,14 +27,14 @@ class SinaSpider(scrapy.Spider):
         item = SinanewsItem()
         judgeurl = response.url
         if str(judgeurl).__contains__('video') or str(judgeurl).__contains__('slide'):
-            pass
+            print ("filter video and slide")
         else:
             try:
                 item["link"] = response.url
                 item["category"] = response.url.split("/")[-2]
                 item["title"] = soup.find('title').getText()
                 item["author"] = soup.find('p', attrs={'class', 'article-editor'}).getText()
-                item["date"]=soup.find('span',id='navtimeSource').getText()
+                item["date"] = soup.find('span', id='navtimeSource').getText()
                 item["content"] = soup.find('div', id='artibody').getText()
             except:
                 pass
@@ -42,4 +43,7 @@ class SinaSpider(scrapy.Spider):
             # item["author"] = unicode(response.xpath('//p[@class="article-editor"]')).encode("GBK")
             # item["content"]=unicode(response.xpath('//div[@id="artibody"]').extract()).encode("gbk")
             # item["title"]=unicode(response.xpath('//title'))
-            yield item
+            if "content" not in item.keys():  # 判断是否含有content
+                print ("empty")
+            else:
+                yield item
--- test.py
+++ test.py
@@ -2,22 +2,10 @@ import urllib
 import urllib2
 from bs4 import BeautifulSoup
 
-
-def begin(self,geturl):
-    self.geturl = geturl
-    soup = BeautifulSoup(urllib.urlopen(geturl), "html.parser")
-    for url in soup.find_all("a"):
-        print url.get('href')
-        endurl=url.get('href')
-        try:
-            if endurl=='http://mall.csdn.net/':
-                return
-            else:
-                begin(endurl)
-        except:
-            return
-
-
-if __name__ == '__main__':
-    surl = 'http://www.csdn.net/'
-    begin(surl)
+def test():
+    mylist=range(3)
+    for i in mylist:
+        yield i
+t=test()
+for i in t:
+    print t
